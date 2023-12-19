8f6a3a64442bc0281aa24369bd487a4bc6f2f93b
Ankit-ui <ankitmani1999@gmail.com>
2023-12-19 12:04:31 +0530

    Argument Passing

--- hello.mod.c
+++ hello.mod.c
@@ -35,13 +35,19 @@ MODULE_INFO(retpoline, "Y");
 
 static const struct modversion_info ____versions[]
 __used __section("__versions") = {
-	{ 0x5b8239ca, "__x86_return_thunk" },
-	{ 0xbdfb6dbb, "__fentry__" },
+	{ 0x26f92ae7, "param_set_int" },
 	{ 0x122c3a7e, "_printk" },
+	{ 0x5b8239ca, "__x86_return_thunk" },
+	{ 0x87a21cb3, "__ubsan_handle_out_of_bounds" },
+	{ 0x35d9915, "param_get_int" },
+	{ 0x2d5eb7d6, "param_array_ops" },
+	{ 0x6e672da8, "param_ops_int" },
+	{ 0x50adc487, "param_ops_charp" },
+	{ 0xbdfb6dbb, "__fentry__" },
 	{ 0xaac58f6c, "module_layout" },
 };
 
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "69B8258B4ED6E3DC10C0D3C");
+MODULE_INFO(srcversion, "0D9F3D922BDA2EDD38C096F");
